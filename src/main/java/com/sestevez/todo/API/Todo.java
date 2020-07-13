d11722a8df367a7b03fa111c2717a26bf1ffea4f
phact <estevezsebastian@gmail.com>
2020-07-13 14:51:55 -0400

    puzzle

--- src/main/java/com/sestevez/todo/API/Todo.java
+++ src/main/java/com/sestevez/todo/API/Todo.java
@@ -8,42 +8,4 @@ public class Todo {
     public Todo() {
     }
 
-    @JsonbProperty()
-    public UUID id;
-    @JsonbProperty()
-    public String title;
-    @JsonbProperty()
-    public boolean completed;
-
-    public Todo(UUID id, String title, boolean completed) {
-        this.id = id;
-        this.title = title;
-        this.completed = completed;
-    }
-
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
 }
--- src/main/java/com/sestevez/todo/AstraTODO.java
+++ src/main/java/com/sestevez/todo/AstraTODO.java
@@ -18,47 +18,12 @@ import java.util.stream.Collectors;
 
 @Path("/api")
 public class AstraTODO {
+
     @Inject
     QuarkusCqlSession cqlSession;
     private String keyspaceName = "free";
     private String tableName = "todolist";
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/todo/{list_id}")
-    public Response getTodos(@PathParam("list_id") String list_id) {
-        PreparedStatement statement = cqlSession
-                .prepare("SELECT * FROM " + keyspaceName + "." + tableName + " where list_id =?");
-
-        BoundStatement bound = statement.bind(list_id);
-
-        ResultSet rs = cqlSession.execute(bound);
-
-        List<Row> rows = rs.all();
-        List<Todo> todos = rows.stream().map(x -> new Todo(x.getUuid("id"), x.getString("title"), x.getBoolean("completed"))).collect(Collectors.toList());
-        return Response.ok(todos).build();
-    }
-
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("/todo/{list_id}")
-    public Response setTodo(@PathParam("list_id") String list_id, Todo todo) {
-        PreparedStatement statement = cqlSession
-                .prepare("INSERT INTO " + keyspaceName + "." + tableName + "(list_id, id, title, completed)VALUES(?,?,?,?)");
-
-        BoundStatement bound = statement.bind(list_id, todo.getId(), todo.getTitle(), todo.isCompleted());
-
-        ResultSet rs = cqlSession.execute(bound);
-
-        boolean successful = rs.wasApplied();
-        if (successful) {
-            return Response.ok().build();
-        }else{
-            return Response.serverError().build();
-        }
-
-    }
 
 
 }
